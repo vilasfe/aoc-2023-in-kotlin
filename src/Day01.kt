13fd27c9d036cc04e33553001e8ec03066d331b2
Fernando Vilas <fernando.e.vilas@gmail.com>
2023-12-02 17:23:00 -0600

    Day01 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,17 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        // each line contains digits + char noise
+        // combine the first and last digit on each line to make a 2 digit num
+        // line might have only one number, so first == last, repeat the digit
+        val data: List<Int> = input.map { it ->
+            val firstDigit = it.first { c -> c.isDigit() }
+            val lastDigit = it.last { c -> c.isDigit() }
+            val num = firstDigit.digitToInt() * 10 + lastDigit.digitToInt()
+            num
+        }
+
+        // sum the results
+        return data.sum()
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +20,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
