c71749d1c1635ee9066af5ce79d864a6a3fb3616
Fernando Vilas <fernando.e.vilas@gmail.com>
2023-12-10 09:17:56 -0600

    Day03 part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -3,18 +3,18 @@ fun main() {
     data class CandidateNumber(val value: Int, val colRange: IntRange, val row: Int)
     data class Symbol(val value: Char, val col: Int, val row: Int)
 
-    fun part1(input: List<String>): Int {
-        val myGrid = input.map { it.toList() }
-        val x_index = myGrid.indices
-        val y_index = myGrid[0].indices
+    data class NumbersAndSymbols(val numbers: List<CandidateNumber>, val symbols: List<Symbol>)
+    fun parseGrid(input: List<List<Char>>): NumbersAndSymbols {
+        val x_index = input.indices
+        val y_index = input[0].indices
         var numbers = mutableListOf<CandidateNumber>()
         var symbols = mutableListOf<Symbol>()
 
         // find all the symbols and their locations
         for (r in x_index) {
-            val locations = myGrid[r].toList().indexesOf { !it.isDigit() && it != '.' }
+            val locations = input[r].toList().indexesOf { !it.isDigit() && it != '.' }
             for (l in locations) {
-                symbols.add(Symbol(myGrid[r][l], l, r))
+                symbols.add(Symbol(input[r][l], l, r))
             }
         }
 
@@ -23,12 +23,12 @@ fun main() {
             var c_start = -1
             var c_end = x_index.max()
             for (c in y_index) {
-                if (c_start == -1 && myGrid[r][c].isDigit()) {
+                if (c_start == -1 && input[r][c].isDigit()) {
                     c_start = c
                 }
-                if (c_start > -1 && !myGrid[r][c].isDigit()) {
+                if (c_start > -1 && !input[r][c].isDigit()) {
                     c_end = c
-                    val num = myGrid[r].subList(c_start, c_end).joinToString(separator="").toInt()
+                    val num = input[r].subList(c_start, c_end).joinToString(separator="").toInt()
                     numbers.add(CandidateNumber(num, c_start..c_end, r))
                     c_end = x_index.max()
                     c_start = -1
@@ -36,10 +36,16 @@ fun main() {
             }
             // Check for number that ends the line
             if (c_start > -1) {
-                val num = myGrid[r].subList(c_start, c_end+1).joinToString(separator="").toInt()
+                val num = input[r].subList(c_start, c_end+1).joinToString(separator="").toInt()
                 numbers.add(CandidateNumber(num, c_start..c_end, r))
             }
         }
+        return NumbersAndSymbols(numbers, symbols)
+    }
+
+    fun part1(input: List<String>): Int {
+        val myGrid = input.map { it.toList() }
+        val (numbers, symbols) = parseGrid(myGrid)
 
         var partNumbers = mutableListOf<Int>()
 
@@ -61,7 +67,25 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val myGrid = input.map { it.toList() }
+        val (numbers, symbols) = parseGrid(myGrid)
+
+        var ratios = mutableListOf<Pair<Int,Int>>()
+
+        // find all the "*" symbols adjacent to exactly 2 numbers
+        for (s in symbols.filter { it.value == '*' } ) {
+            var ratio = mutableListOf<Int>()
+            for (n in numbers.filter { it.row >= s.row - 1 && it.row <= s.row + 1 } ) {
+                if (s.col >= n.colRange.min() - 1 && s.col <= n.colRange.max()) {
+                    ratio.add(n.value)
+                }
+            }
+            if (ratio.size == 2) {
+                ratios.add(Pair<Int,Int>(ratio[0], ratio[1]))
+            }
+        }
+
+        return ratios.fold(0) { sum, i -> sum + i.first * i.second }
     }
 
     // test if implementation meets criteria from the description, like:
@@ -71,5 +95,8 @@ fun main() {
 
     val input = readInput("Day03")
     part1(input).println()
+
+    println(part2(testInput))
+    check(part2(testInput) == 467835)
     part2(input).println()
 }
