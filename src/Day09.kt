82f3b868f4a774d94f0bc4a57bb35be04d4d4371
Fernando Vilas <fernando.e.vilas@gmail.com>
2023-12-17 09:05:59 -0600

    Day09 part 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,28 +1,38 @@
 fun main() {
+
+    fun extrapolate(seq: List<Int>): Pair<Int, Int> {
+        var differences = mutableListOf<List<Int>>()
+        differences.add(seq)
+        while (! differences.last().all { it == 0 }) {
+            differences.add(differences.last().zipWithNext() { s1, s2 -> s2 - s1 })
+        }
+        var nextVal = 0
+        var prevVal = 0
+        for (d in differences.lastIndex-1 downTo 0) {
+            nextVal += differences[d].last()
+            prevVal = differences[d].first() - prevVal
+        }
+        return Pair(prevVal, nextVal)
+    }
+
     fun part1(input: List<String>): Int {
         val historyInput = input.map {
             it.split("\\s+".toRegex()).map { i -> i.toInt() }
         }
 
-        var total = 0
-        for (h in historyInput) {
-            var differences = mutableListOf<List<Int>>()
-            differences.add(h)
-            while (! differences.last().all { it == 0 }) {
-                differences.add(differences.last().zipWithNext() { s1, s2 -> s2 - s1 })
-            }
-            var nextVal = 0
-            for (d in differences.lastIndex-1 downTo 0) {
-                nextVal += differences[d].last()
-            }
-            total += nextVal
-        }
+        val total = historyInput.fold(0) { sum, it -> sum + extrapolate(it).second }
 
         return total
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val historyInput = input.map {
+            it.split("\\s+".toRegex()).map { i -> i.toInt() }
+        }
+
+        val total = historyInput.fold(0) { sum, it -> sum + extrapolate(it).first }
+
+        return total
     }
 
     // test if implementation meets criteria from the description, like:
@@ -31,5 +41,7 @@ fun main() {
 
     val input = readInput("Day09")
     part1(input).println()
+
+    check(part2(testInput) == 2)
     part2(input).println()
 }
