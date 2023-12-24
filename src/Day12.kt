a0e84c531e28de3fa1a71cc75d7cb0dbed89396f
Fernando Vilas <fernando.e.vilas@gmail.com>
2023-12-23 19:49:10 -0600

    Day 12 part 2

--- src/Day12.kt
+++ src/Day12.kt
@@ -18,11 +18,7 @@ fun main() {
 
             // if this is the rest of the record is the last group then done
             if (conditions.length == groups[0]) {
-                if (groups.size == 1) {
-                    return 1
-                } else {
-                    return 0
-                }
+                return if (groups.size == 1) 1 else 0
             }
 
             // if the character after this group can be a separator then recurse
@@ -77,14 +73,80 @@ fun main() {
         return combos.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun detectCombinationsPart2(conditions: String, groups: List<Int>): Long {
+        // Setup the list of good springs with a leading good one
+        val goodSprings = mutableListOf('.')
+        goodSprings.addAll(conditions.dropWhile { it == '.' }.toList())
+
+        // possible combos starts with 1 at each location
+        var possibleCombos = MutableList(goodSprings.size + 1) { 1L }
+
+        // Set the possible combos to 0 if the spring is damaged
+        for ((idx, _) in goodSprings.withIndex().dropWhile { (_, c) -> c != '#' }) {
+            possibleCombos[idx + 1] = 0
+        }
+
+        // Now check for each group and start updating
+        for (g in groups) {
+            // create a new row in the test matrix
+            val nextCombos = MutableList(goodSprings.size + 1) { 0L }
+            var runSize = 0
+
+            for ((idx, c) in goodSprings.withIndex().drop(1)) {
+                if (c == '.') {
+                    runSize = 0
+                } else {
+                    runSize += 1
+                }
+
+                // evaluate the rest of the list in this row
+                val groupCanFit = runSize >= g
+                val prevIdx = (idx - g).coerceAtLeast(0) // make sure indexes are nonnegative
+                val hasDamageGap = goodSprings[prevIdx] != '#'
+                val isNotDamaged = c != '#'
+                val dmgCanFit = groupCanFit && hasDamageGap
+
+                nextCombos[idx+1] =
+                    if (isNotDamaged && dmgCanFit) {
+                        nextCombos[idx] + possibleCombos[prevIdx]
+                    } else if (c == '#' && dmgCanFit) {
+                        possibleCombos[prevIdx]
+                    } else if (isNotDamaged) {
+                        nextCombos[idx]
+                    } else {
+                        0L
+                    }
+            }
+
+            // move to the next layer
+            possibleCombos = nextCombos
+        }
+
+        return possibleCombos.last()
+    }
+
+    fun part2(input: List<String>): Long {
+        val parsedLists = input.map { it.split("\\s+".toRegex()) }
+        val parsedPairs = parsedLists.map { s ->
+            Pair((s[0]+"?").repeat(4) + s[0],
+                List(5) { s[1].split(",").map { g -> g.toInt() }}.flatten() )
+        }
+
+        //parsedPairs[0].println()
+
+        val combos = parsedPairs.map { it -> detectCombinationsPart2(it.first, it.second)
+            //.also { println("---") }
+        }
+        return combos.sum()
+
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day12_test")
     println("Part1 test: ${part1(testInput)}")
     check(part1(testInput) == 21)
+    println("Part2 test: ${part2(testInput)}")
+    check(part2(testInput) == 525152L)
 
     val input = readInput("Day12")
     part1(input).println()
