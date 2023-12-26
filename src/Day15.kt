75cd655bf985928ba0927a187992e759ecebe8e3
Fernando Vilas <fernando.e.vilas@gmail.com>
2023-12-25 20:32:40 -0600

    Day 15 part 2

--- src/Day15.kt
+++ src/Day15.kt
@@ -4,6 +4,12 @@ fun main() {
         return total
     }
 
+    fun sumList(input: List<Pair<String, Int>>): Int {
+        return input.foldIndexed(0) { idx, t, i ->
+            t + (idx+1) * i.second
+        }
+    }
+
     fun part1(input: List<String>): Int {
         val parsed = input[0].split(",")
 
@@ -13,13 +19,48 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val parsed = input[0].split(",")
+
+        var hashMap = mutableMapOf<Int,MutableList<Pair<String,Int>>>()
+
+        parsed.forEach { p ->
+            val key = p.split("=|-".toRegex())[0]
+            val op = p[key.length]
+
+            val hashedKey = reindeerHash(key)
+
+            var tmp = hashMap[hashedKey] ?: mutableListOf<Pair<String,Int>>()
+
+            if (op == '-') {
+                // remove the value from tmp
+                tmp.filter { it.first == key }.forEach { tmp.remove(it) }
+            } else if (op == '=') {
+                val value = p.substring(key.length+1).toInt()
+                // if there is a lens with the value then replace it
+                val idx = tmp.indexOfFirst { it.first == key }
+                if (idx > -1) {
+                    tmp[idx] = Pair(key, value)
+                } else {
+                // else, add this one to the end
+                    tmp.add(Pair(key, value))
+                }
+            }
+            hashMap[hashedKey] = tmp
+        }
+
+        var total = hashMap.map { (k,v) ->
+            (k + 1) * sumList(v)
+        }.sum()
+
+        return total
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day15_test")
     println("Part 1 test: ${part1(testInput)}")
     check(part1(testInput) == 1320)
+    println("Part 2 test: ${part2(testInput)}")
+    check(part2(testInput) == 145)
 
     val input = readInput("Day15")
     part1(input).println()
